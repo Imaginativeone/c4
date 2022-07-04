99b0a3de3d12027f83d150eadaa0819279de8afd
Doug Franklin <doug@dougfranklin.com>
2022-07-04 12:58:14 -0400

    Flexible Paths

--- models/dbmgr.js
+++ models/dbmgr.js
@@ -1,4 +1,18 @@
+// TODO Get App Path
 const sqlite = require('better-sqlite3-with-prebuilds');
-const db = new sqlite('./mytest.db');
+const path = require('path');
+
+const totalname = __dirname;
+
+const somePath = [];
+somePath.push(totalname);
+somePath.push(path.dirname(totalname));
+// somePath.push(path.basename(totalname));
+// const somePath = path.extname(totalname);
+
+console.log('database manager', somePath);
+
+// const db = new sqlite('./mytest.db');
+const db = new sqlite(somePath[1] + '/mytest.db');
 
 exports.db = db;
